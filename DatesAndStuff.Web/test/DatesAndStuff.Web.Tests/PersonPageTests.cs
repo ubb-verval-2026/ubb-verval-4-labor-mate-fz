1fb44eee7f5999b3b5199aa28f7e3821d64adee9
mate <mate.fazekas@stud.ubbcluj.ro>
2026-05-13 13:26:34 +0300

    test failing on synchronization

--- DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
@@ -101,6 +101,7 @@ public class PersonPageTests
     [TestCase(10, 5500)]
     [TestCase(0, 5000)]
     [TestCase(-5, 4750)]
+    [TestCase(-10, 4500)]
     public void Person_SalaryIncrease_ShouldIncrease(double percentage, double expectedSalary)
     {
         // Arrange
